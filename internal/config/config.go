c8b643f9e61bf2f093b8c903ed28e1c8fc2e1ff0
jonathanhu237 <jonathanhu237@gmail.com>
2025-12-10 10:36:20 +0800

    feat(config): add config loading from environment variables
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

---
+++ internal/config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import "github.com/caarlos0/env/v11"
+
+type Config struct {
+	InitAdmin InitAdminConfig `envPrefix:"INIT_ADMIN_"`
+	Database  DatabaseConfig  `envPrefix:"DATABASE_"`
+}
+
+type InitAdminConfig struct {
+	Username string `env:"USERNAME"`
+	Password string `env:"PASSWORD"`
+	Email    string `env:"EMAIL"`
+	Name     string `env:"NAME"`
+}
+
+type DatabaseConfig struct {
+	User     string `env:"USER"`
+	Password string `env:"PASSWORD"`
+	Host     string `env:"HOST"`
+	Port     int    `env:"PORT"`
+	DB       string `env:"DB"`
+}
+
+func Load() (*Config, error) {
+	cfg := &Config{}
+	if err := env.ParseWithOptions(cfg, env.Options{RequiredIfNoDef: true}); err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
