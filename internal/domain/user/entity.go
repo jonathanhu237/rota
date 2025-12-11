a402f961d517ad5336eed54085eaa2d099a7957c
jonathanhu237 <jonathanhu237@gmail.com>
2025-12-11 23:30:01 +0800

    feat(user): add json tags to hide sensitive fields
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- internal/domain/user/entity.go
+++ internal/domain/user/entity.go
@@ -3,14 +3,14 @@ package user
 import "time"
 
 type User struct {
-	ID           string
-	Username     string
-	PasswordHash string
-	Name         string
-	Email        string
-	IsAdmin      bool
-	IsActive     bool
-	Version      int
-	CreatedAt    time.Time
-	UpdatedAt    time.Time
+	ID           string    `json:"id"`
+	Username     string    `json:"username"`
+	PasswordHash string    `json:"-"`
+	Name         string    `json:"name"`
+	Email        string    `json:"email"`
+	IsAdmin      bool      `json:"is_admin"`
+	IsActive     bool      `json:"is_active"`
+	Version      int       `json:"-"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
 }
